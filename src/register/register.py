10cb9fc8554c23e5c2b64a6cc0ec4567a2db223e
Scott Guyton <kiljoy001@gmail.com>
2023-08-14 23:23:26 -0700

    added function to check cert validation, working on register function

--- src/register/register.py
+++ src/register/register.py
@@ -1,61 +1,71 @@
 import subprocess
+from frozendict import frozendict
 from src.namecoin_command import execute_namecoin_command
 
-COMMANDS = {
-    "generate_ca_key": (
-        "openssl",
-        "genpkey",
-        "-algorithm",
-        "RSA",
-        "-out",
-        "ca_private_key.pem",
-    ),
-    "generate_ca_cert": (
-        "openssl",
-        "req",
-        "-new",
-        "-x509",
-        "-key",
-        "ca_private_key.pem",
-        "-out",
-        "ca_certificate.pem",
-        "-days",
-        "365",
-    ),
-    "generate_key": (
-        "openssl",
-        "genpkey",
-        "-algorithm",
-        "RSA",
-        "-out",
-        "{cert_name}_private_key.pem",
-    ),
-    "generate_cert": (
-        "openssl",
-        "req",
-        "-new",
-        "-key",
-        "{cert_name}_private_key.pem",
-        "-out",
-        "{cert_name}.csr",
-    ),
-    "sign_cert": (
-        "openssl",
-        "x509",
-        "-req",
-        "-in",
-        "{cert_name}.csr",
-        "-CA",
-        "ca_certificate.pem",
-        "-CAkey",
-        "ca_private_key.pem",
-        "-CAcreateserial",
-        "-out",
-        "{cert_name}_certificate.pem",
-        "-days",
-        "365",
-    ),
-}
+COMMANDS = frozendict(
+    {
+        "generate_ca_key": (
+            "openssl",
+            "genpkey",
+            "-algorithm",
+            "RSA",
+            "-out",
+            "ca_private_key.pem",
+        ),
+        "generate_ca_cert": (
+            "openssl",
+            "req",
+            "-new",
+            "-x509",
+            "-key",
+            "ca_private_key.pem",
+            "-out",
+            "ca_certificate.pem",
+            "-days",
+            "365",
+        ),
+        "generate_key": (
+            "openssl",
+            "genpkey",
+            "-algorithm",
+            "RSA",
+            "-out",
+            "{cert_name}_private_key.pem",
+        ),
+        "generate_cert": (
+            "openssl",
+            "req",
+            "-new",
+            "-key",
+            "{cert_name}_private_key.pem",
+            "-out",
+            "{cert_name}.csr",
+        ),
+        "sign_cert": (
+            "openssl",
+            "x509",
+            "-req",
+            "-in",
+            "{cert_name}.csr",
+            "-CA",
+            "ca_certificate.pem",
+            "-CAkey",
+            "ca_private_key.pem",
+            "-CAcreateserial",
+            "-out",
+            "{cert_name}_certificate.pem",
+            "-days",
+            "365",
+        ),
+        "validate_cert": {
+            "openssl",
+            "x509",
+            "-in",
+            "{cert_path}",
+            "-noout",
+        },
+    }
+)
 
 
 def run_command(command_type, cert_name=None):
@@ -73,9 +83,9 @@ def run_command(command_type, cert_name=None):
 
 
 def generate_x509_cert(cert_name):
-    run_command("generate_key", cert_name=f"{cert_name}_private_key.pem")
-    run_command("generate_cert", cert_name=f"{cert_name}.csr")
-    run_command("sign_cert", cert_name=f"{cert_name}_certificate.pem")
+    run_command("generate_key", cert_name=cert_name)
+    run_command("generate_cert", cert_name=cert_name)
+    run_command("sign_cert", cert_name=cert_name)
 
 
 def generate_ca():
@@ -83,5 +93,17 @@ def generate_ca():
     run_command("generate_ca_cert")
 
 
+def is_valid_certificate(cert_path):
+    try:
+        run_command("validate_cert", cert_name=cert_path)
+        return True
+    except Exception:
+        return False
+
+
 def register_new_domain(domain_name):
-    pass
+    # check balance
+    min_balance = 0.015
+    balance = execute_namecoin_command("getbalance")
+    if balance[balance] > min_balance:
+        response = execute_namecoin_command("namenew")
